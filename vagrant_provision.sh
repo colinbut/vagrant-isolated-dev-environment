76f788839eda22628088aaa2d63a8ca20d20818b
Colin But <colinbut@users.noreply.github.com>
2016-07-24 10:23:50 +0100

    install tomcat examples and git

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -8,9 +8,12 @@ sudo apt-get install -y apache2
 
 echo "Installing Tomcat"
 sudo apt-get install -y tomcat7
-
 echo "Installing Tomcat docs"
 sudo apt-get install -y tomcat7-docs
-
-echo "Installing Tomcat7 administration web apps"
+echo "Installing Tomcat administration web apps"
 sudo apt-get install -y tomcat7-admin
+echo "Installing Tomcat examples"
+sudo apt-get install -y tomcat7-examples
+
+echo "Installing Git"
+sudo apt-get install -y git
