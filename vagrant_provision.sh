c8194b7f726d4453e67cfe80f9ebef9551ea6f7d
Colin But <colinbut@users.noreply.github.com>
2016-07-24 10:27:27 +0100

    install Java

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -17,3 +17,12 @@ sudo apt-get install -y tomcat7-examples
 
 echo "Installing Git"
 sudo apt-get install -y git
+
+echo "Installing Java 7"
+sudo apt-get install -y software-properties-common python-software-properties
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+echo "Setting environment variables for Java 7"
+sudo apt-get install -y oracle-java7-set-default
