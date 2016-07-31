29385928b5ea674c159bee22de3fc1f663ac444f
Colin But <colinbut@users.noreply.github.com>
2016-07-31 13:00:25 +0100

    fixed package manager to using yum in centos

--- centOS/vagrant_provision.sh
+++ centOS/vagrant_provision.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# upating apt-get first
-sudo apt-get update
+# upating yum first
+sudo yum update
 
 #Apache Http Web Server
 if [ ! -f /usr/lib/apache2/mpm-worker/apache2 ];
 then
 	echo "Installing Apache"
-	sudo apt-get install -y apache2
+	sudo yum install -y apache2
 else
 	echo "Apache already installed - skipping"
 fi
@@ -16,13 +16,13 @@ fi
 if [ ! -f /etc/init.d/tomcat7 ];
 then
 	echo "Installing Tomcat"
-	sudo apt-get install -y tomcat7
+	sudo yum install -y tomcat7
 	echo "Installing Tomcat docs"
-	sudo apt-get install -y tomcat7-docs
+	sudo yum install -y tomcat7-docs
 	echo "Installing Tomcat administration web apps"
-	sudo apt-get install -y tomcat7-admin
+	sudo yum install -y tomcat7-admin
 	echo "Installing Tomcat examples"
-	sudo apt-get install -y tomcat7-examples
+	sudo yum install -y tomcat7-examples
 else
 	echo "Tomcat 7 already installed - skipping"
 fi
@@ -32,13 +32,13 @@ fi
 if [ ! -f /usr/lib/jvm/java-7-oracle/bin/java ];
 then
 	echo "Installing Java 7"
-	sudo apt-get install -y software-properties-common python-software-properties
+	sudo yum install -y software-properties-common python-software-properties
 	echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 	sudo add-apt-repository ppa:webupd8team/java -y
-	sudo apt-get update
-	sudo apt-get install oracle-java7-installer
+	sudo yum update
+	sudo yum install oracle-java7-installer
 	echo "Setting environment variables for Java 7"
-	sudo apt-get install -y oracle-java7-set-default
+	sudo yum install -y oracle-java7-set-default
 else
 	echo "Java 7 already installed - skipping"
 fi
@@ -47,7 +47,7 @@ fi
 if [ ! -f /usr/share/maven/bin/mvn ];
 then
 	echo "Installing Maven"
-	sudo apt-get install -y maven
+	sudo yum install -y maven
 else
 	echo "Maven already installed - skipping"
 fi
@@ -56,7 +56,7 @@ fi
 if [ ! -f /usr/bin/git ];
 then
 	echo "Installing Git"
-	sudo apt-get install -y git
+	sudo yum install -y git
 else
 	echo "Git already installed - skipping"
 fi
@@ -65,7 +65,7 @@ fi
 if [ ! -f /usr/sbin/nginx ];
 then
 	echo "Installing NGINX"
-	sudo apt-get install -y nginx
+	sudo yum install -y nginx
 else
 	echo "NGINX already installed - skipping"
 fi
@@ -74,10 +74,10 @@ fi
 if [ ! -f /usr/sbin/mysqld ];
 then
 	echo "Installing MySQL Server"
-	sudo apt-get install -y debconf-utils
+	sudo yum install -y debconf-utils
 	debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 	debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
-	sudo apt-get install -y mysql-server
+	sudo yum install -y mysql-server
 else
 	echo "Mysql server already installed - skipping"
 fi
