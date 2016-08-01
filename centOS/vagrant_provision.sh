49969b7b72e27bb81626cf69a311aaff8892b65d
Colin But <colinbut@users.noreply.github.com>
2016-08-01 21:32:02 +0100

    fixing mysql installation by adding mysql community repo

--- centOS/vagrant_provision.sh
+++ centOS/vagrant_provision.sh
@@ -41,13 +41,19 @@ sudo systemctl enable tomcat
 
 
 #Java
-if [ ! -f /usr/lib/jvm/java-7-oracle/bin/java ];
+if [ ! -f /opt/jdk1.7.0_79/bin/java ];
 then
-	echo "Installing Java 7"
+	echo "Downloading Java 7"
 	cd /opt/
 	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz"
 	tar xzf jdk-7u79-linux-x64.tar.gz
+	echo "Installing Java 7"
 	cd /opt/jdk1.7.0_79/
+	alternatives --install /usr/bin/java java /opt/jdk1.7.0_79/bin/java 2
+	alternatives --install /usr/bin/jar jar /opt/jdk1.7.0_79/bin/jar 2
+	alternatives --install /usr/bin/javac javac /opt/jdk1.7.0_79/bin/javac 2
+ 	alternatives --set jar /opt/jdk1.7.0_79/bin/jar
+ 	alternatives --set javac /opt/jdk1.7.0_79/bin/javac
 else
 	echo "Java 7 already installed - skipping"
 fi
@@ -81,7 +87,8 @@ else
 	echo "NGINX already installed - skipping"
 fi
 
-
+echo "Setting up MySQL Community Repo"
+sudo rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
 
 #MySQL Server
 if [ ! -f /usr/sbin/mysqld ];
